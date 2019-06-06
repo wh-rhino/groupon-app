eab10488b279458fab9977fabac0d8d91bf65faf
Paulo Ossandon <paulo.ossandon.ica@gmail.com>
2019-06-06 08:41:43 -0400

    Feat: Adaptation function to class and constructor incorporation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import 'bulma';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Look movie</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              <input type="text" value={this.state.value} className="input" onChange={this.handleChange} placeholder="Search" /> 
+              <input className="button is-info" type="submit" value="Submit" />
+            </p>
+          </header>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
