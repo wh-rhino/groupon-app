64a7bfc5ccbc4606970e732ad39a2d89d9a6ddef
Paulo Ossandon <paulo.ossandon.ica@gmail.com>
2019-06-06 16:30:30 -0400

    Feat: Integration REST APIs omdbapi

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import logo from './logo.png';
 import './App.css';
 import 'bulma';
 
+const baseURL = process.env.REACT_APP_OMDAPI_URL;
+const baseURLKEY = process.env.REACT_APP_OMDAPI_KEY;
+
 class App extends React.Component {
+  
   constructor(props) {
     super(props);
     this.state = {value: ''};
@@ -12,15 +16,33 @@ class App extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  state = {
+    movies: []
+  }
+  
   handleChange(event) {
     this.setState({value: event.target.value});
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    // example call API of omdbapi: curl 'http://www.omdbapi.com/?t=Batman&apikey=434202b0'
+    // return: {"Title":"Batman","Year":"1989","Rated":"PG-13","Released":"23 Jun 1989","Runtime":"126 min","Genre":"Action, Adventure","Director":"Tim Burton","Writer":"Bob Kane (Batman characters), Sam Hamm (story), Sam Hamm (screenplay), Warren Skaaren (screenplay)","Actors":"Michael Keaton, Jack Nicholson, Kim Basinger, Robert Wuhl","Plot":"The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker.","Language":"English, French, Spanish","Country":"USA, UK","Awards":"Won 1 Oscar. Another 8 wins & 26 nominations.","Poster":"https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"7.6/10"},{"Source":"Rotten Tomatoes","Value":"71%"},{"Source":"Metacritic","Value":"69/100"}],"Metascore":"69","imdbRating":"7.6","imdbVotes":"311,189","imdbID":"tt0096895","Type":"movie","DVD":"25 Mar 1997","BoxOffice":"N/A","Production":"Warner Bros. Pictures","Website":"N/A","Response":"True"}
     event.preventDefault();
+    this.componentDidMount(this.state.value);
   }
 
+  componentDidMount(title) {
+    var API = baseURL+title+baseURLKEY;
+    console.log(API);
+    fetch(API)
+    .then(res => res.json())
+    .then((data) => {
+      console.log(data);
+      this.setState({ movies: data })
+    })
+    .catch(console.log)
+  }
+  
   render(){
     return (
       <div className="App">
